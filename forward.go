e33721a11268a24f6894e49d367151926e83141d
riet <loli_520@126.com>
2023-11-22 12:40:37 +0800

    update port forward

--- forward.go
+++ forward.go
@@ -9,40 +9,33 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-type PortForwarder interface {
-	PortForward() error
-	PortForwardContext(context.Context) error
-}
-
 type portForward struct {
 	client *ssh.Client
 }
 
-// listenLocal listen at host:port
-func (p portForward) listenLocal(host, port string) (net.Listener, error) {
-	return net.Listen("tcp", host+":"+port)
+// listenLocal listen at addr
+func (p portForward) listenLocal(addr string) (net.Listener, error) {
+	return net.Listen("tcp", addr)
 }
 
-// dailRemote let remote peer dail host:port
-func (p portForward) dailRemote(host, port string) (net.Conn, error) {
-	return p.client.Dial("tcp", host+":"+port)
+// dailRemote let remote peer dail addr
+func (p portForward) dailRemote(addr string) (net.Conn, error) {
+	return p.client.Dial("tcp", addr)
 }
 
-// listenRemote let remote peer listen at host:port
-func (p portForward) listenRemote(host, port string) (net.Listener, error) {
-	return p.client.Listen("tcp", host+":"+port)
+// listenRemote let remote peer listen at addr
+func (p portForward) listenRemote(addr string) (net.Listener, error) {
+	return p.client.Listen("tcp", addr)
 }
 
-// dailLocal dail host:port
-func (p portForward) dailLocal(host, port string) (net.Conn, error) {
-	return net.Dial("tcp", host+":"+port)
+// dailLocal dail addr
+func (p portForward) dailLocal(addr string) (net.Conn, error) {
+	return net.Dial("tcp", addr)
 }
 
-func (p portForward) forward(src, dst io.ReadWriteCloser) error {
+func (p portForward) forward(src, dst io.ReadWriteCloser) {
 	var wg sync.WaitGroup
 	var o sync.Once
-	var err1 error
-	var err2 error
 	closeReader := func() {
 		_ = src.Close()
 		_ = dst.Close()
@@ -50,145 +43,98 @@ func (p portForward) forward(src, dst io.ReadWriteCloser) error {
 
 	wg.Add(2)
 	go func() {
-		_, err1 = io.Copy(src, dst)
+		io.Copy(src, dst)
 		o.Do(closeReader)
 		wg.Done()
 	}()
 
 	go func() {
-		_, err2 = io.Copy(dst, src)
+		io.Copy(dst, src)
 		o.Do(closeReader)
 		wg.Done()
 	}()
 	wg.Wait()
-	if err1 != nil {
-		return err1
-	}
-	return err2
 }
 
-type LocalToRemote struct {
-	f          *portForward
-	localHost  string
-	localPort  string
-	remoteHost string
-	remotePort string
+// PortForwarder
+// local and remote is ip:port
+type PortForwarder struct {
+	f      *portForward
+	local  string
+	remote string
 }
 
-func (l LocalToRemote) ListenLocal() (net.Listener, error) {
-	return l.f.listenLocal(l.localHost, l.localPort)
+func (p PortForwarder) LocalForwardToRemote() error {
+	return p.LocalForwardToRemoteContext(context.Background())
 }
 
-func (l LocalToRemote) DailRemote() (net.Conn, error) {
-	return l.f.dailRemote(l.remoteHost, l.remotePort)
-}
-
-// PortForward  local host:port -> remote host:port
-func (l LocalToRemote) PortForward() error {
-	return l.PortForwardContext(context.Background())
-}
-
-func (l LocalToRemote) PortForwardContext(ctx context.Context) error {
-	ch := make(chan error)
-	localListener, err := l.ListenLocal()
+func (p PortForwarder) LocalForwardToRemoteContext(ctx context.Context) error {
+	l, err := p.f.listenLocal(p.local)
 	if err != nil {
 		return err
 	}
-	defer localListener.Close()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case err := <-ch:
-			return err
-		default:
-			local, err := localListener.Accept()
-			if err != nil {
-				return err
-			}
-			go func() {
-				remote, err := l.DailRemote()
-				if err != nil {
-					ch <- err
-				}
-				if err := l.f.forward(local, remote); err != nil {
-					ch <- err
-				}
-			}()
-		}
-	}
-}
-
-func NewLocalToRemoteForward(client *ssh.Client, localHost, localPort, remoteHost, remotePort string) PortForwarder {
-	return &LocalToRemote{
-		f:          &portForward{client: client},
-		localHost:  localHost,
-		localPort:  localPort,
-		remoteHost: remoteHost,
-		remotePort: remotePort,
-	}
-}
-
-type RemoteToLocal struct {
-	f          *portForward
-	localHost  string
-	localPort  string
-	remoteHost string
-	remotePort string
-}
-
-func (r RemoteToLocal) ListenRemote() (net.Listener, error) {
-	return r.f.listenRemote(r.remoteHost, r.remotePort)
-}
-
-func (r RemoteToLocal) DailLocal() (net.Conn, error) {
-	return r.f.dailLocal(r.localHost, r.localPort)
-}
-
-// PortForward  remote host:port -> local host:port
-func (r RemoteToLocal) PortForward() error {
-	return r.PortForwardContext(context.Background())
-}
-
-func (r RemoteToLocal) PortForwardContext(ctx context.Context) error {
+	defer l.Close()
 	ch := make(chan error)
-	remoteListener, err := r.ListenRemote()
+
+	go func() {
+		for {
+			local, err := l.Accept()
+			if err != nil {
+				ch <- err
+				return
+			}
+			remote, err := p.f.dailRemote(p.remote)
+			if err != nil {
+				ch <- err
+				return
+			}
+			go p.f.forward(local, remote)
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-ch:
+		return err
+	}
+}
+
+func (p PortForwarder) RemoteForwardToLocal() error {
+	return p.RemoteForwardToLocalContext(context.Background())
+}
+
+func (p PortForwarder) RemoteForwardToLocalContext(ctx context.Context) error {
+	r, err := p.f.listenRemote(p.remote)
 	if err != nil {
 		return err
 	}
-	defer remoteListener.Close()
+	defer r.Close()
+	ch := make(chan error)
 
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case err := <-ch:
-			return err
-		default:
-			remote, err := remoteListener.Accept()
+	go func() {
+		for {
+			remote, err := r.Accept()
 			if err != nil {
-				return err
+				ch <- err
+				return
 			}
-			go func() {
-				local, err := r.DailLocal()
-				if err != nil {
-					ch <- err
-				}
-				if err := r.f.forward(local, remote); err != nil {
-					ch <- err
-				}
-			}()
+			local, err := p.f.dailLocal(p.local)
+			if err != nil {
+				ch <- err
+				return
+			}
+			go p.f.forward(remote, local)
 		}
+	}()
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-ch:
+		return err
 	}
+
 }
 
-func NewRemoteToLocalForward(client *ssh.Client, localHost, localPort, remoteHost, remotePort string) PortForwarder {
-	return &RemoteToLocal{
-		f:          &portForward{client: client},
-		localHost:  localHost,
-		localPort:  localPort,
-		remoteHost: remoteHost,
-		remotePort: remotePort,
-	}
+func NewPortForwarder(client *ssh.Client, local, remote string) *PortForwarder {
+	return &PortForwarder{f: &portForward{client: client}, local: local, remote: remote}
 }
