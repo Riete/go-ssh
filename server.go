e13e75946e2176aff6e6b95a62aa806a0c1655f6
riet <loli_520@126.com>
2023-12-25 15:57:10 +0800

    support connect to serve via socks5 proxy

--- server.go
+++ server.go
@@ -1,18 +1,22 @@
 package ssh
 
 import (
-	"fmt"
+	"context"
+	"net"
 	"time"
+
+	"golang.org/x/net/proxy"
 
 	"golang.org/x/crypto/ssh"
 )
 
 type Server struct {
-	ip   string
-	port string
+	ip     string
+	port   string
+	dialer func(ctx context.Context, network, addr string) (c net.Conn, err error)
 }
 
-func (s Server) connect(username string, timeout time.Duration, methods ...AuthMethod) (*ssh.Client, error) {
+func (s *Server) connect(username string, timeout time.Duration, methods ...AuthMethod) (*ssh.Client, error) {
 	config := &ssh.ClientConfig{User: username, HostKeyCallback: ssh.InsecureIgnoreHostKey(), Timeout: timeout}
 	for _, method := range methods {
 		if auth, err := method(); err != nil {
@@ -21,7 +25,21 @@ func (s Server) connect(username string, timeout time.Duration, methods ...AuthM
 			config.Auth = append(config.Auth, auth)
 		}
 	}
-	return ssh.Dial("tcp", fmt.Sprintf("%s:%s", s.ip, s.port), config)
+	addr := s.ip + ":" + s.port
+	if s.dialer != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		conn, err := s.dialer(ctx, "tcp", addr)
+		if err != nil {
+			return nil, err
+		}
+		c, chans, reqs, err := ssh.NewClientConn(conn, addr, config)
+		if err != nil {
+			return nil, err
+		}
+		return ssh.NewClient(c, chans, reqs), nil
+	}
+	return ssh.Dial("tcp", addr, config)
 }
 
 func (s *Server) OpenSession() SessionExecutor {
@@ -38,6 +56,16 @@ func (s *Server) OpenIShell() InteractiveShell {
 
 func (s *Server) OpenClient(username string, timeout time.Duration, methods ...AuthMethod) (*ssh.Client, error) {
 	return s.connect(username, timeout, methods...)
+}
+
+func (s *Server) SetSocks5Proxy(addr string, auth *proxy.Auth) error {
+	d, err := proxy.SOCKS5("tcp", addr, auth, proxy.Direct)
+	if err != nil {
+		return err
+	}
+	xd := d.(proxy.ContextDialer)
+	s.dialer = xd.DialContext
+	return nil
 }
 
 func NewServer(ip, port string) *Server {
